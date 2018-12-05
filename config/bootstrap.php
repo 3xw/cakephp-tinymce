1b8a7b63103391b62bb4cb27c07fd569ef04e0bc
Wallef Antoine <antoine.wallef@gmail.com>
2018-12-05 15:59:29 +0100

    load bootstrap config

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -2,3 +2,6 @@
 use Cake\Core\Configure;
 
 Configure::load('Trois/Tinymce.tinymce');
+collection((array) Configure::read('Trois/Tinymce.config'))->each(function ($file) {
+    Configure::load($file,'default',true);
+});
