c725d0f6331b831a8b4a0068ca809672ae8596ae
awallef <antoine.wallef@gmail.com>
2020-01-16 09:56:28 +0100

    Update Plugin.php

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,13 +1,21 @@
 <?php
+declare(strict_types=1);
+
 namespace Trois\Tinymce;
 
 use Cake\Core\BasePlugin;
+use Cake\Routing\RouteBuilder;
 use Cake\Core\PluginApplicationInterface;
 
 class Plugin extends BasePlugin
 {
-  public function bootstrap(PluginApplicationInterface $app)
+  public function bootstrap(PluginApplicationInterface $app): void
   {
     parent::bootstrap($app);
+  }
+
+  public function routes(RouteBuilder $routes): void
+  {
+    parent::routes($routes);
   }
 }
